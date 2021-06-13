a23bf8819693c18ff111dd0ff6e3847a3b9ebb71
linklelo <1096056364@qq.com>
2021-06-13 20:34:28 +0800

    基本框架完成

---
+++ app/core/config.py
@@ -0,0 +1,44 @@
+
+import logging
+import sys
+from typing import List
+
+from loguru import logger
+from starlette.config import Config
+from starlette.datastructures import CommaSeparatedStrings, Secret
+
+from app.core.logging import InterceptHandler
+
+API_PREFIX = ""
+
+JWT_TOKEN_PREFIX = "Token"  # noqa: S105
+VERSION = "0.0.0"
+
+config = Config(".env")
+
+DEBUG: bool = config("DEBUG", cast=bool, default=False)
+
+DATABASE_URL: str = config("DB_CONNECTION", cast=str)
+MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
+MIN_CONNECTIONS_COUNT: int = config("MIN_CONNECTIONS_COUNT", cast=int, default=10)
+
+SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
+
+PROJECT_NAME: str = config("PROJECT_NAME", default="FastAPI example application")
+ALLOWED_HOSTS: List[str] = config(
+    "ALLOWED_HOSTS",
+    cast=CommaSeparatedStrings,
+    default="",
+)
+
+# logging configuration
+
+LOGGING_LEVEL = logging.DEBUG if DEBUG else logging.INFO
+LOGGERS = ("uvicorn.asgi", "uvicorn.access")
+
+logging.getLogger().handlers = [InterceptHandler()]
+for logger_name in LOGGERS:
+    logging_logger = logging.getLogger(logger_name)
+    logging_logger.handlers = [InterceptHandler(level=LOGGING_LEVEL)]
+
+logger.configure(handlers=[{"sink": sys.stderr, "level": LOGGING_LEVEL}])
